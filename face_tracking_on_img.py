c15e0bac61f3f9d4aecbc9ce54f51a2470509c7c
geekydoodle <102618044+geekydoodle@users.noreply.github.com>
2022-04-01 15:34:49 +0530

    Detect on Images

---
+++ face_tracking_on_img.py
@@ -0,0 +1,45 @@
+######## Face Tracking (Img) using OpenCV #########
+
+# Author: George
+# Youtube Channel : https://www.youtube.com/channel/UC-j647KCqjQKE50Npx9TZsg
+
+# Description: 
+# This code uses OpenCV to do Face Tracking on a Img.
+
+# Importing OpenCV.
+
+import cv2
+
+# Path to Img.
+
+img = cv2.imread('/home/pi/george/coding_place/python/projects/face_tracking/img.jpg')
+
+# Path to Cascade Classifier.
+
+cascade = cv2.CascadeClassifier('/home/pi/george/coding_place/python/projects/face_tracking/haarcascade_frontalface_default.xml')
+
+# Converting BGR to GRAY.
+    
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Getting the faces.
+
+faces = cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x, y, w, h) in faces:
+        
+    # Drawing a rectangle around the face.
+        
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 4)
+    
+# Displaing the Img.
+
+cv2.imshow('Face Tracking', img)
+
+# Infinite loop.
+
+cv2.waitKey(0)
+
+# Destroying all the windows.
+
+cv2.destroyAllWindows()
